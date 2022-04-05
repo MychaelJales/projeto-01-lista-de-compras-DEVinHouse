23c509345f223a985d6054576c7c53ca3a7386c9
MychaelJales <mychael_duarte@hotmail.com>
2022-04-05 17:02:24 -0300

    feat: criando metodos addState, updateList e clearInput

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const btnAdd = document.getElementById('btn-add');
+const inputProduct = document.getElementById('input-product');
+const list = document.getElementById('list');
+
+// const state = [];
+
 class ProductsList {
   #state;
   constructor() {
@@ -5,16 +11,42 @@ class ProductsList {
     this.id = 1;
   }
 
-  set setState (name) {
-    const { id } = this;
-    const newProduct = { name, id }
-    this.#state = [...this.#state, newProduct];
-    this.id += 1;
+  set setState (state) {
+    this.#state = state;
   }
 
   get getState() {
     return this.#state;
   }
+
+  updateList() {
+    list.innerHTML = '';
+    const state = this.getState;
+    state.forEach((item) => {
+      const li = document.createElement('li');
+      li.innerText = item.name;
+      list.appendChild(li);
+    });
+  }
+
+  clearInput() {
+    inputProduct.value = '';
+  }
+
+  addState() {
+    const { id } = this;
+    const { value } = inputProduct;
+    const oldState = this.getState;
+    const newProduct = { name: value, id }
+    const newState = [...oldState, newProduct];
+    this.setState = newState;
+    this.id += 1;
+    this.updateList();
+    this.clearInput();
+  }
+
 }
 
-const productsList = new ProductsList();
+const productsList1 = new ProductsList();
+
+btnAdd.addEventListener('click', () => productsList1.addState());
