d255f0c49c6d43d378ee1cda1b32ef5399918c2d
MychaelJales <mychael_duarte@hotmail.com>
2022-04-05 16:10:30 -0300

    feat: criando classe productsList e implementando getters e setters do stado da aplicacao

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('funciona');
+class ProductsList {
+  #state;
+  constructor() {
+    this.#state = [];
+    this.id = 1;
+  }
+
+  set setState (name) {
+    const { id } = this;
+    const newProduct = { name, id }
+    this.#state = [...this.#state, newProduct];
+    this.id += 1;
+  }
+
+  get getState() {
+    return this.#state;
+  }
+}
+
+const productsList = new ProductsList();
