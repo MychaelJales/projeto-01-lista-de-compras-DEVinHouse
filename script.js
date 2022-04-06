07c23a18cb6f43010c6444b82f7af8d9222ec2b1
MychaelJales <mychael_duarte@hotmail.com>
2022-04-05 21:22:22 -0300

    feat: criando metodo removePrice, controllerModal, checkboxClick and sumAll

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const btnAdd = document.getElementById('btn-add');
 const btnRmvAll = document.getElementById('btn-rmv-all');
 const btnRmvSelect = document.getElementById('btn-rmv-select');
+const btnInsertModal = document.getElementById('btn-insert-modal');
 const inputProduct = document.getElementById('input-product');
 const list = document.getElementById('list');
+const inputPrice = document.getElementById('input-price');
 
 // const state = [];
 
@@ -11,6 +13,8 @@ class ProductsList {
   constructor() {
     this.#state = [];
     this.id = 1;
+    this.idOpenModal = 0;
+    this.sumTotal = 0;
   }
 
   set setState (state) {
@@ -42,6 +46,47 @@ class ProductsList {
     });
   }
 
+  removePrice(itemId) {
+    const oldState = this.getState;
+    const newState = oldState.map((item) => {
+      if (item.id === +itemId) {
+        const value = 0;
+        return {...item, value };
+      } else {
+        console.log({ v: 'dentro do else', item });
+        return item;
+      }
+    });
+    this.setState = newState;
+    console.log(this.getState);
+    this.sumAll();
+  }
+
+  controllerModal(checked, itemId) {
+    if (checked) {
+      this.idOpenModal = itemId;
+      myModal.show()
+    } else {
+      this.idOpenModal = itemId;
+      this.removePrice(itemId);
+    }
+  }
+
+  checkboxClick(itemId) {
+    const oldState = this.getState;
+    const newState = oldState.map((item) => {
+      if (item.id === +itemId) {
+        const toogle = !item.checked;
+        return {...item, checked: toogle};
+      } else {
+        return item;
+      }
+    });
+    const { checked } = newState.find((item) => item.id === +itemId)
+    this.setState = newState;
+    this.controllerModal(checked, itemId);
+  }
+
   updateList() {
     list.innerHTML = '';
     const state = this.getState;
@@ -55,6 +100,7 @@ class ProductsList {
         <button id="btn-rmv-${item.id}" type="button" class="btn btn-outline-danger btn-sm">🗑️</button>
       `;
       list.appendChild(li);
+      document.getElementById(`item-${item.id}`).addEventListener('click',() => productsList.checkboxClick(item.id));
       document.getElementById(`btn-rmv-${item.id}`).addEventListener('click',() => productsList.removeById(item.id));
     });
   }
@@ -67,7 +113,7 @@ class ProductsList {
     const { id } = this;
     const { value } = inputProduct;
     const oldState = this.getState;
-    const newProduct = { name: value, id }
+    const newProduct = { name: value, id, checked: false, value: 0 }
     const newState = [...oldState, newProduct];
     this.setState = newState;
     this.id += 1;
@@ -75,6 +121,27 @@ class ProductsList {
     this.clearInput();
   }
 
+  addPrice() {
+    const oldState = this.getState;
+    const { idOpenModal } = this;
+    const newState = oldState.map((item) => {
+      if (item.id === +idOpenModal) {
+        const value = Number(inputPrice.value);
+        return {...item, value };
+      } else {
+        return item;
+      }
+    });
+    this.setState = newState;
+    this.sumAll();
+    inputPrice.value = '';
+  }
+
+  sumAll() {
+    const state = this.getState;
+    this.sumTotal = state.reduce((acc, cur) => acc + cur.value, 0);
+    console.log(this.sumTotal);
+  }
 }
 
 const productsList = new ProductsList();
@@ -82,3 +149,12 @@ const productsList = new ProductsList();
 btnAdd.addEventListener('click', () => productsList.addState());
 btnRmvAll.addEventListener('click', () => productsList.removeAll());
 btnRmvSelect.addEventListener('click', () => productsList.removeSelected());
+btnInsertModal.addEventListener('click', () => productsList.addPrice())
+
+// var AmyModal = document.getElementById('AmyModal');
+var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+
+/* AmyModal.addEventListener('click', function () {
+  myModal.show()
+}) */
+
