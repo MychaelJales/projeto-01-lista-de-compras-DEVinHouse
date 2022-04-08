872e1a00774798f521640586a51ab13bba39213f
MychaelJales <mychael_duarte@hotmail.com>
2022-04-08 20:51:22 -0300

    test: adicionando teste do req 3

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -1,5 +1,6 @@
 const PAGE_TITLE = 'Minha Lista de Compras';
 const ABA_TITLE = 'Lista de Compras';
+const INPUT_PRODUCT = 'input#input-product';
 
 describe('1 - Um título na aba do navegador, para que o usuário encontre a sua aplicação no meio das várias abas que constantemente mantém abertas;', () => {
   beforeEach(() => {
@@ -20,5 +21,20 @@ describe('2 - Um cabeçalho dentro da página, para que o usuário saiba facilme
   
   it('Será verificado se a página possui uma tag `header` com o conteúdo `Minha Lista de Compras`', () => {
     cy.get('header').contains(PAGE_TITLE);
+  });
+});
+
+describe('3 - Um campo de texto para digitar o nome do produto a ser adicionado à lista', () => {
+  beforeEach(() => {
+    cy.viewport(1366, 768);
+    cy.visit('./index.html');
+  });
+  
+  it('Será verificado se a página possui uma tag `input` com `id = input-product`', () => {
+    cy.get(INPUT_PRODUCT).should('exist');
+  });
+
+  it('Será verificado se a página possui uma tag `input` com `placeholder = Insira sua nova compra`', () => {
+    cy.get('input[placeholder="Insira sua nova compra"]').should('exist');
   });
 });
